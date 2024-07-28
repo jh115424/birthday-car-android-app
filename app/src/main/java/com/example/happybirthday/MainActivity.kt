6ef33382644c7918570512f6bf7767dc015d6687
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-07-28 12:42:28 -0500

    first android project in a while

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.material3.MaterialTheme
+
+import androidx.compose.material3.Surface
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+
+//                        modifier = Modifier.padding(innerPadding)
+
+                }
+            }
+        }
+    }
+
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        HappyBirthdayTheme {
+
+        }
+    }
+}
